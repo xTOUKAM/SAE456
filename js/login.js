34f190ed6b7e28f5e39c7800e2765d5f25fe2062
HOULLEGATTE Tom <houllegatte.tom@gmail.com>
2023-12-12 09:20:11 +0100

    Adding connexion.html + login.js | Modification of css

---
+++ js/login.js
@@ -0,0 +1,21 @@
+/* Récupération des éléments du formulaire */
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Comparaison avec les comptes prédéfinis
+    if(username.value == "admin" && password.value == "admin") {
+        window.location.href = "index.html";
+    } else if(username.value == "entreprise" && password.value == "entreprise") {
+        window.location.href = "index.html";
+    } else if(username.value == "association" && password.value == "association") {
+        window.location.href = "index.html";
+    } else if(username.value == "mairie" && password.value == "mairie") {
+        window.location.href = "index.html";
+    } else {
+        alert("Identifiant ou mot de passe incorrect");
+    }
+});
+
